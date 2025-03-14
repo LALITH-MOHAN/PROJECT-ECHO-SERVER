7a6155560314e2f9770549d98a53092c259f8bca
LALITH-MOHAN <lalith22004@gmail.com>
2025-03-14 15:35:24 +0530

    echo_server

--- ECHO_server/echo_server.c
+++ ECHO_server/echo_server.c
@@ -1,67 +1,98 @@
 #include <stdio.h>
+#include <time.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <arpa/inet.h>
 
 #define PORT 8080
-#define BUFFER_SIZE 1024
 
-int main() {
-    int server_fd, new_socket;
-    struct sockaddr_in address;
-    socklen_t addrlen = sizeof(address);
-    char buffer[BUFFER_SIZE] = {0};
-    char response[] = 
-        "HTTP/1.0 200 OK\r\n"
-        "Content-Type: text/html\r\n"
-        "Content-Length: 8\r\n"
-        "\r\n"
-        "Welcome!";
+struct message 
+{
+    char msg[1024];  //message
+    char time[10];  //time stamp
+    char date[11]; //present date
+    int ct;       //count for no of messages echoed
+    int uptime;  //uptime in seconds
+};
 
-    // 1. Create socket
-    server_fd = socket(AF_INET, SOCK_STREAM, 0);
-    if (server_fd == 0) {
-        perror("Socket failed");
+void encrp_decrp(char *msg, char *key); //encrypt & decrypt
+
+int main() 
+{
+    int server_fd = socket(AF_INET, SOCK_DGRAM, 0); // socket creation
+    if (server_fd == -1) 
+    {
+        perror("SOCKET CREATION FAILED");
         exit(EXIT_FAILURE);
     }
+    printf("SERVER SOCKET CREATED FD: %d\n", server_fd);
 
-    // 2. Configure server address
-    address.sin_family = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons(PORT);
+    struct sockaddr_in server_addr, client_addr;
+    socklen_t client_len = sizeof(client_addr);
+    
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+    server_addr.sin_port = htons(PORT);
 
-    // 3. Bind socket to the port
-    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
-        perror("Bind failed");
+    if (bind(server_fd, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) // bind the server socket
+    {
+        perror("BINDING FAILED");
         exit(EXIT_FAILURE);
     }
+    printf("SERVER BOUND TO PORT %d\n", PORT);
 
-    // 4. Start listening for connections
-    if (listen(server_fd, 10) < 0) {
-        perror("Listen failed");
-        exit(EXIT_FAILURE);
-    }
-    printf("Server is listening on port %d...\n", PORT);
+    int total_messages = 0; // message count
+    time_t start_time = time(NULL); // start of server.
 
-    while (1) {
-        // 5. Accept a connection
-        new_socket = accept(server_fd, (struct sockaddr *)&address, &addrlen);
-        if (new_socket < 0) {
-            perror("Accept failed");
+    while (1) 
+    {
+        struct message det;
+        char buff[1024] = {0};
+        
+        int rec = recvfrom(server_fd, buff, sizeof(buff) - 1, 0, (struct sockaddr*)&client_addr, &client_len);
+        if (rec < 0) 
+        {
+            perror("RECEIVING FAILED");
             continue;
         }
-
-        // 6. Read request
-        read(new_socket, buffer, BUFFER_SIZE);
-        printf("Received request:\n%s\n", buffer);
-
-        // 7. Send a basic response
-        write(new_socket, response, sizeof(response) - 1);
         
-        // 8. Close connection
-        close(new_socket);
-    }
+        buff[rec] = '\0';
+        time_t current_time;
+        struct tm *time_info;
+        time(&current_time);
+        time_info = localtime(&current_time); // current time
+        sprintf(det.time, "%02d:%02d:%02d", time_info->tm_hour, time_info->tm_min, time_info->tm_sec); // current timestamp
+        sprintf(det.date, "%d/%d/%d", time_info->tm_mday, time_info->tm_mon + 1, time_info->tm_year + 1900); // present date
+        
+        #define KEY "key" //secret key
+        encrp_decrp(buff, KEY); // decrypt the message
+        strncpy(det.msg, buff, sizeof(det.msg) - 1);
+        det.msg[sizeof(det.msg) - 1] = '\0';
+        total_messages++;
 
+        det.ct = htonl(total_messages); 
+        det.uptime = htonl((int)(current_time - start_time));
+
+        encrp_decrp(det.msg, KEY); // encrypt the message before echoing
+        sendto(server_fd, (char*)&det, sizeof(det), 0, (struct sockaddr*)&client_addr, client_len);
+        
+        encrp_decrp(det.msg, KEY);
+
+        printf("MESSAGE SENT: %s at %s on %s, Uptime: %d seconds\n", det.msg, det.time, det.date, ntohl(det.uptime));
+    }
+    printf("SERVER CLOSED\n");
+    close(server_fd);
     return 0;
+}
+
+void encrp_decrp(char *msg, char *key) 
+{
+    int keyl = strlen(key);
+    for (int i = 0; msg[i] != '\0'; i++) 
+    {
+        msg[i] ^= key[i % keyl];
+    }
 }
